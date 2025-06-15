9513a1cdc11249c6b5a39d3b97b64a76858a5999
Anugup333 <139995580+Anugup333@users.noreply.github.com>
2025-06-15 11:24:48 +0000

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,34 @@
+// const name = "hitesh"
+const repoCount =  50 
+
+// console.log(name + repoCount + " Value ")
+
+// console.log(`Hello my name is ${name} and my repo count ${repoCount} Value `);
+
+const gameName = new String("Anuj Gupta")
+// console.log(gameName[0])
+
+// console.log(gameName.length)
+// console.log(gameName.toLowerCase())
+// console.log(gameName.charAt(9))
+// console.log(gameName.indexOf('G'))
+console.log(gameName)
+
+const newgameName = gameName.substring(0,5)
+
+// console.log(newgameName);
+// console.log(gameName.slice(-6,4))
+
+const newString = "      Anuj     "
+// console.log(newString);
+// console.log(newString.trim())
+
+const url = "https://hitesh.com/hitesh%20 choudhary"
+
+// console.log(url.replace(" ",""));
+
+const name = "Anuj-Gupta-Surabhi-Gupta"
+
+splitname = name.split("-")
+
+console.log(typeof splitname)
