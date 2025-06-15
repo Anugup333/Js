13f8f03d30703e6fd0d8200ccd0d2ae6d6f6f9f4
Anugup333 <139995580+Anugup333@users.noreply.github.com>
2025-06-15 09:07:59 +0000

    conversion

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,26 @@
+let score = "12a"
+
+// console.log(typeof score);
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "12" ==> 12
+// "22am" ==> NaN
+// true ==> 1
+
+
+let isLoggedIn = ""
+
+
+let booleanisLoggenIn = Boolean(isLoggedIn)
+
+// console.log(booleanisLoggenIn)
+
+// 1 ==> true 0==> false
+// "" ==> false
+// "hitesh" ==> true
+
+
