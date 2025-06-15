caa6c81c5a007a38b35b67f56acc9239ec7c3193
Anugup333 <139995580+Anugup333@users.noreply.github.com>
2025-06-15 10:37:20 +0000

    memory

--- 01_basic/datatype_summary.js
+++ 01_basic/datatype_summary.js
@@ -26,9 +26,43 @@ const myFunction = function(){
     console.log(" Hello World ")
 }
 
-console.log(typeof bigNumber)
+// console.log(typeof bigNumber)
 
-console.log(typeof heros)
-console.log(typeof myObj)
-console.log(typeof myFunction)
+// console.log(typeof heros)
+// console.log(typeof myObj)
+// console.log(typeof myFunction)
 
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Memory in javascript 
+// Stack ( Primitive Type ) ( Copy ) 
+// Heap( Non- Primitive type ) (Reference)  
+
+let myYoutubename = "Anuj"
+
+let anothername = myYoutubename
+
+// console.log(anothername)
+// console.log(myYoutubename)
+// anothername = "Chai aur code "
+// console.log(anothername)
+// console.log(myYoutubename)
+
+// Because of Copy (Stack) of Primitive type 
+
+let user = {
+    email: "anuj@gmail.com",
+    upi : "upiid",
+}
+let user2 = user
+
+console.log(user)
+console.log(user2)
+
+user.email = "manoj@gmail.com"
+
+console.log(user)
+console.log(user2)
+
+// Because of Reference (Heap) of Non-Primitive type 
