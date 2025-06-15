8b80fa4c5f69792616e21211e142e2b5c6aa99d3
Anugup333 <139995580+Anugup333@users.noreply.github.com>
2025-06-15 10:20:00 +0000

    data type

---
+++ 01_basic/04_operation.js
@@ -0,0 +1,29 @@
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log(1 + 2 + "2")
+// console.log("1" + 2 + 2) 
+
+// console.log(+true)
+// console.log(+"")
+
+
+
+let a = 1;
+let b = 2;
+
+let c = 3 - (a = b + 1);
+
+alert( a ); // 3
+alert( c ); // 0
+
+
+let a = 0;
+alert( Boolean(a) ); // false
+
+let b = "0";
+alert( Boolean(b) ); // true
+
+alert(a == b); // true!
+
+
+
---
+++ 01_basic/datatype_summary.js
@@ -0,0 +1,29 @@
+// Primitive  Data type :
+
+// 7 Type: String, Number , Boolean, null, undefined, symbol, 
+// BigInt
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId )  // false
+
+const bigNumber = 23453565468n
+console.log(typeof bigNumber);  // bigint
+
+
+// Reference Data Type :
+
+// Array , Objects , Functions 
+
+const heros = ['shaktiman','naagraj','doga']
+
+let myObj = {
+    name: "Anuj",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log(" Hello World ")
+}
+
+
